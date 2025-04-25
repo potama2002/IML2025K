7730445164d961dc1b582f8968404b6b6a8d8ee1
竹村颯人 <cguh0064@mail4.doshisha.ac.jp>
2025-04-25 13:40:34 +0900

    Add files via upload

---
+++ takemura/chapter01/chapter04/knock31.py
@@ -0,0 +1,8 @@
+from knock30 import parse_mecab_result  # 30番の関数を再利用
+
+if __name__ == "__main__":
+    sentences = parse_mecab_result("merosu.txt.mecab")
+    for sentence in sentences:
+        for morph in sentence:
+            if morph['pos'] == '動詞':
+                print(morph['surface'])
